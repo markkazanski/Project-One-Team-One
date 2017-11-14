f644411a8ef8ec912a777a4251dfce419c7fe63a
Mark Kazanski <markkazanski@gmail.com>
2017-11-13 20:57:55 -0600

    merge conflict app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -118,16 +118,3 @@ $(document).ready(function () {
 	});
 
 });
-<<<<<<< HEAD
-		//display description and photo from BustBuy.js
-		//$("#prod-title").html(""); //pass in title variable from BestBuy.js
-		//$("#prod-description").html(""); //pass in description variable from BestBuy.js
-		//$("#prod-image").html(""); //pass in image variable from BestBuy.js
-
-		//display ratings from WebHose.js
-		//$("#ratings").html(""); //pass in variable from Webhose.js
-
-		//display videos from Youtube.js
- 		//$("#videos").html("") //pass in object variable from Youtube API
-=======
->>>>>>> upstream/master
